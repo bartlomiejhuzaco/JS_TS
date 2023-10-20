b68f1c108cc64b3c77a037120a8efe9453e7ee6a
BartlomiejHuzaCO <bartlomiej.huza@gmail.com>
2023-10-20 12:34:01 +0200

    Lekcja_3

---
+++ dest/script.js
@@ -0,0 +1,34 @@
+"use strict";
+// ZMIENNA
+/*
+    ZMIENNA
+    LEKCJA
+    W TOKU
+*/
+let nazwaKtoraJestBardzoDobra = "";
+let nazwa_ktora_jest_bardzo_dobra = "";
+let _x10_ = 10;
+let _ = "";
+// STAŁA
+const g = 9.81;
+// TYPY
+// PRYMITYWNE
+// string
+let s = "String";
+// number
+let n = 150;
+// boolean
+let b = true; // false
+// false => undefined, null, 0, ""
+let empty = undefined;
+let a2;
+let b2 = null;
+let c2 = 0;
+// Złożone
+// tablica - array
+let array = [];
+let array2 = [];
+// obiekt - object
+let obj = {
+    "email": "domain@cos.pl"
+};
---
+++ src/script.ts
@@ -0,0 +1,43 @@
+// ZMIENNA
+
+/*
+    ZMIENNA
+    LEKCJA
+    W TOKU
+*/
+
+let nazwaKtoraJestBardzoDobra: string = "";
+let nazwa_ktora_jest_bardzo_dobra = "";
+let _x10_ = 10;
+let _ = "";
+
+// STAŁA
+
+const g = 9.81;
+
+// TYPY
+
+// PRYMITYWNE
+// string
+let s: string = "String";
+
+// number
+let n: number = 150;
+
+// boolean
+let b: boolean = true; // false
+
+// false => undefined, null, 0, ""
+let empty: undefined = undefined;
+let a2: undefined;
+let b2: null = null;
+let c2 = 0;
+
+// Złożone
+// tablica - array
+let array: Array<string> = [];
+let array2: string[] = [];
+// obiekt - object
+let obj: object = {
+    "email": "domain@cos.pl"
+};
