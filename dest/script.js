d5d6f964e85776a5a0ded320beb6b1a937ed0764
BartlomiejHuzaCO <bartlomiej.huza@gmail.com>
2023-10-21 08:36:37 +0200

    Lekcja_8

--- dest/script.js
+++ dest/script.js
@@ -1,55 +1,14 @@
 "use strict";
-let month = "DECEMBER";
-switch (month) {
-    case "JANUARY":
-        //CODE
-        console.log("1");
-        break; // return;
-    case "FEBRUARY":
-        //CODE
-        console.log("2");
-        break; // return;
-    case "MARCH":
-        //CODE
-        console.log("3");
-        break; // return;
-    case "APRIL":
-        //CODE
-        console.log("Prima aprilis");
-        break; // return;
-    case "MAY":
-        //CODE
-        console.log("MAYO");
-        break; // return;
-    case "JUNE":
-        //CODE
-        console.log("THE SAME AS JULY");
-        break; // return;
-    case "JULY":
-        //CODE
-        console.log("IT'S JULY");
-        break; // return;
-    case "AUGUST":
-        //CODE
-        console.log("ENDING OF HOLIDAY");
-        break; // return;
-    case "SEPTEMBER":
-        //CODE
-        console.log("SCHOOL");
-        break; // return;
-    case "OCTOBER":
-        //CODE
-        console.log("10");
-        break; // return;
-    case "NOVEMBER":
-        //CODE
-        console.log("12");
-        break; // return;
-    case "DECEMBER":
-        //CODE
-        console.log("FELIZ NAVIDAD");
-        break; // return;
-    default:
-        // CODE
-        break;
-}
+// function callMe(nr: number): void {
+//     console.log(` I call to you! ${nr}`);
+// }
+// callMe(15623156);
+// const data = (obj: Object) => {
+//     console.log(obj.email || "");
+// }
+(function () {
+    console.log("ONLOADING, HELLO");
+})();
+(() => {
+    console.log("ONLOADING, HELLO");
+})();
