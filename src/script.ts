15e4baf7bac4a50e4ef90fbf6f99550ef0c0973d
BartlomiejHuzaCO <bartlomiej.huza@gmail.com>
2023-10-26 10:29:30 +0200

    Lekcja_17

--- src/script.ts
+++ src/script.ts
@@ -1,51 +1,43 @@
+// SET
 
-// ITERABLE
-const a = [1, 2, 3, 4];
-const it: Iterator<number> = a[Symbol.iterator]();
-console.log(it);
-while (true) {
-   let next = it.next()
-   if (!next.done) {
-       console.log(next.value)
-   } else {
-      break;
-   }
-}
+// const set: Set<number> = new Set([15, 20, 30, 15, 20, 10, 10, 10, 10]);
+// console.log(set);
 
-// SIMPLE IMPLEMENTATION
-const obj = {
-    email: "email@domain",
-    fullname: "Name",
-    age: 50,
-    [Symbol.iterator]: function(){
-        return {
-            value: this,
-            done: false,
-        }
-    }
-};
+// const setGenerator = set.values();
+// const setGeneratorKeys = set.keys();
 
-console.log(obj[Symbol.iterator]().value);
+// for (let item of setGenerator) {
+//     console.log(item);
+// }
 
-// Generator
+// console.log(set.has(15));
 
-function * generator() {
-    console.log("1");
-    console.log("2");
-    console.log("3");
-    console.log("4");
-    yield 5; // { value: 5, done: false }
-    console.log("1");
-    console.log("2");
-    console.log("3");
-    console.log("4");
-    yield 10; // { value: 10, done: false }
 
-    return 15; // { value: 15, done: false }
-}
+// MAP
+// const map: Map<string, Array<string>> = new Map([
+//     ["key1", ["a", "b", "c"]],
+//     ["key2", ["d", "e", "f"]],
+// ]);
+// console.log(map);
 
-const gen: Generator<number, number, unknown> = generator();
-console.log(gen.next());
-console.log(gen.next());
-console.log(gen.next());
-console.log(gen.next());
+// map.forEach((value, key, map) => {
+//     console.log(value);
+//     console.log(key);
+//     console.log("HEllo");
+// });
+
+// MAP FUNCTION
+// const arr = [15, 20, 30, 15, 20, 10, 10, 10, 10].map((value, index, array) => {
+//     console.log(value, index);
+//     console.log(array);
+
+//     return value;
+// });
+
+// console.log(arr);
+
+// SYMBOL
+const sym1 = Symbol("uuid");
+const sym2 = Symbol("Email");
+
+Symbol.iterator;
